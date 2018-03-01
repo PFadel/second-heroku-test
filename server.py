21d2610cb8d6b919c5bcb07d8209cf90e722bd0b
Fadel <pfadel@stone.com.br>
2018-03-01 19:20:38 -0300

    :lock: adiciona validação de header

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -11,6 +11,11 @@ port = int(os.environ.get("PORT", 5000))
 
 @app.route('/hello')
 def hello():
+    if not request.headers.get("cooler_token"):
+        return jsonify({
+            "message": "You need to send me a cooler token!"
+        }), 401
+
     return jsonify({"message": "I'm even cooler"})
 
 
